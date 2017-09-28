7923a0d9c708992f96b284b72fc46f268735f3ea
somehuman22 <resiewrc@gmail.com>
2017-09-28 10:35:24 -0700

    Update mpg.java

--- mpg.java
+++ mpg.java
@@ -1,16 +1,20 @@
 public class Car {
+    //declarations
     int myStartMiles;
     int myEndMiles;
     double myGallonsUsed;
-
+    
+    //constructor
     public Car(int odometerReading) {
         this.myStartMiles = odometerReading;
     }
-
+    
+    //get mpg
     double calculateMPG() {
         return (this.myEndMiles - this.myStartMiles) / this.myGallonsUsed;
     }
-
+    
+    //?????
     void fillUp(int odometerReading, double gallons) {
         this.myGallonsUsed += gallons;
         this.myEndMiles += odometerReading;
